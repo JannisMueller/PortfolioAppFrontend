401b520ec3432fdbefe21be26416214f3a2158c1
JannisMueller <jannis.mueller@hotmail.de>
2021-05-31 15:54:34 +0200

    adding new chart

--- src/components/Overview.vue
+++ src/components/Overview.vue
@@ -3,34 +3,46 @@
   <div>
 
   <div>
+    <div class="overview">
   <table>
-    <tr>
-      <td>Assets</td>
+    <tr class="column-header">
+      <td></td>
       <td>Value</td>
       <td>Gain</td>
       <td>% of Portfolio</td>
+
     </tr>
 
-    <tr v-for="asset in assets"
+    <tr  v-for="asset in assets"
     :key="asset.id">
-    <td>{{asset.nameOfAsset}}</td>
-      <td>{{ (asset.currentValue)}}</td>
-      <td>{{asset.gain + "%"}}</td>
-      <td>{{ (asset.currentValue/totalValuePortfolio *100).toFixed(1) + "%"}}</td>
+    <td class="row-header">{{asset.nameOfAsset}}</td>
+      <td :class="{green: asset.gain>1, red: asset.gain<0, orange: asset.gain <= 1}">{{ (asset.currentValue)}}</td>
+      <td :class="{green: asset.gain>1, red: asset.gain<0, orange: asset.gain <= 1}">{{asset.gain + "%"}}</td>
+      <td :class="{green: asset.gain>1, red: asset.gain<0, orange: asset.gain <= 1}">{{ (asset.currentValue/totalValuePortfolio *100).toFixed(1) + "%"}}</td>
     </tr>
     <tr>
-      <td>Total Value of Portfolio </td>
+      <td class="bottom-line" >Current Value of Portfolio </td>
+
     <td>{{totalValuePortfolio}}</td>
-      <td>{{totalGainInPortfolio + "%"}}</td>
+      <td :class="{green: totalGainInPortfolio>0, red: totalGainInPortfolio<0}">{{totalGainInPortfolio + "%"}}</td>
       <td>100%</td>
     </tr>
-    <button @click="submitTotalValue"> Save total Value of Portfolio</button>
+  <tr class="column-header">
+    <td>Total gain in Stocks</td>
+    <td></td>
+    <td>{{totalGainOfStocksInPortfolio}}</td>
+    <td></td>
+
+  </tr>
+
+    <button @click="submitTotalValue()"> Save total Value of Portfolio</button>
   </table>
+    </div>
 
     <br>
     <br>
 
-    <table>
+  <table>
 
       <tr>
         <td>Total Value over time</td>
@@ -38,73 +50,79 @@
       </tr>
 
 
-    <tr v-for="metric in metrics"
-    :key="metric.id"
+    <tr v-for="value in totalValues"
+    :key="value.id"
     >
-      <td>{{ (metric.totalValue).toFixed(2) }}</td>
-      <td>{{ (metric.totalGain).toFixed(2) + "%" }}</td>
+      <td>{{ (value.totalValue).toFixed(2) }}</td>
+      <td>{{ (value.totalGain).toFixed(2) }}</td>
+
     </tr>
     </table>
 
   </div>
 
-    <br>
-    <br>
 
-  <div>
-
-    <table v-if="showDetails">
-
-      <tr>
-        <td>Bucket 1</td>
-        <td>Bucket 2</td>
-        <td>Bucket 3</td>
-        <td>Bucket 4</td>
-      <tr class="bg-emerald-200">
-        <td>
-          <ol>
-          <li v-for="asset in bucketOne" :key="asset.id">{{asset.nameOfAsset}} {{asset.currentValue}}</li>
-          </ol>
-        </td>
-
-        <td>
-          <ol>
-          <li v-for="asset in bucketTwo" :key="asset.id">{{asset.nameOfAsset}} {{asset.currentValue}}</li>
-          </ol>
-        </td>
-
-        <td>
-          <ol>
-          <li v-for="asset in bucketThree" :key="asset.id">{{asset.nameOfAsset}} {{asset.currentValue}}</li>
-          </ol>
-        </td>
-
-        <td>
-          <ol v-for="asset in bucketFour" :key="asset.id">
-          <li> {{asset.nameOfAsset}} {{asset.currentValue}}</li>
-          </ol>
-        </td>
-      </tr>
-      <tr>
-        <td>{{totalValueBucketOne}}</td>
-        <td>{{totalValueBucketTwo}}</td>
-        <td>{{totalValueBucketThree}}</td>
-        <td>{{totalValueBucketFour}}</td>
-      </tr>
-      <button @click="togglePortfolio">Close Bucket-view</button>
-    </table>
-    <button v-if="!showDetails" @click="togglePortfolio">Show Bucket-view</button>
-
-  </div>
 
     <br>
     <br>
 
   <div class="chart-container">
+    <line-chart :chart-data="dataCollection"></line-chart>
+    <br>
+    <br>
     <pie-chart :data="chartData" :options="chartOptions"></pie-chart>
 
-    <line-chart :chart-data="datacollection"></line-chart>
+
   </div>
+
+    <br>
+    <br>
+
+    <div>
+
+      <table v-if="showDetails">
+
+        <tr>
+          <td>Bucket 1</td>
+          <td>Bucket 2</td>
+          <td>Bucket 3</td>
+          <td>Bucket 4</td>
+        <tr class="bg-emerald-200">
+          <td>
+            <ol>
+              <li v-for="asset in bucketOne" :key="asset.id">{{asset.nameOfAsset}} {{asset.currentValue}}</li>
+            </ol>
+          </td>
+
+          <td>
+            <ol>
+              <li v-for="asset in bucketTwo" :key="asset.id">{{asset.nameOfAsset}} {{asset.currentValue}}</li>
+            </ol>
+          </td>
+
+          <td>
+            <ol>
+              <li v-for="asset in bucketThree" :key="asset.id">{{asset.nameOfAsset}} {{asset.currentValue}}</li>
+            </ol>
+          </td>
+
+          <td>
+            <ol v-for="asset in bucketFour" :key="asset.id">
+              <li> {{asset.nameOfAsset}} {{asset.currentValue}}</li>
+            </ol>
+          </td>
+        </tr>
+        <tr>
+          <td>{{totalValueBucketOne}}</td>
+          <td>{{totalValueBucketTwo}}</td>
+          <td>{{totalValueBucketThree}}</td>
+          <td>{{totalValueBucketFour}}</td>
+        </tr>
+        <button @click="togglePortfolio">Close Bucket-view</button>
+      </table>
+      <button v-if="!showDetails" @click="togglePortfolio">Show Bucket-view</button>
+
+    </div>
 
   </div>
 </template>
@@ -121,23 +139,22 @@ export default {
   components: {LineChart, PieChart},
   data: function () {
     return {
-      datacollection: {
-        labels: ["week 20/2021", "week 21/2021", "week 22/2021"],
+      dataCollection: {
+        labels: [],
         datasets: [
           {
-            data: [45.6, 35.6, 49.6],
+            data: [0, 969, 970],
             label: "Total value of Portfolio (in thousands)",
             borderColor: "#3e95cd",
             fill: false
           },
           {
-            data: [34, 28, 43],
-            label: "Total Gain of Portfolio in %",
-            borderColor: "#8e5ea2",
+            data: [0, 0.9, 1],
+            label: "Total value of Portfolio (in thousands)",
+            borderColor: "green",
             fill: false
-          },
-
-        ]
+          }
+          ]
       },
       chartOptions: {
         hoverBorderWidth: 20,
@@ -146,17 +163,19 @@ export default {
       chartData: {
         hoverBackgroundColor: "red",
         hoverBorderWidth: 10,
-        labels: ["Stock", "Short-term bonds", "long-term bonds", "Gold"],
+        labels: ["Stock", "Short-term bonds", "long-term bonds", "Pension" ,"Gold"],
         datasets: [
           {
             label: "Data One",
             backgroundColor: ["#41B883", "#E46651", "#00D8FF", "#00D8FF"],
-            data: [33, 67, 0, 0]
+            data: [15.4,31.0,0,52.6,0]
           }
         ]
       },
       assets: [],
       metrics: [],
+      totalGains: [],
+      totalValues: [],
       nameOfAsset: '',
       typeOfAsset: '',
       bucket: null,
@@ -165,8 +184,11 @@ export default {
       totalValue: null,
       totalGain: null,
       totalValueEachBucket: null,
-      showDetails: false
-
+      showDetails: false,
+      shareStocks: 13,
+      shareBondsShort: 13,
+      shareBondsLong: 34,
+      sharePension: 56
 
     }
   },
@@ -192,17 +214,21 @@ export default {
           })
           .then((data) => {
             this.metrics = data
+            this.totalValues = data
           })
           .then(() => {
             this.fetchData();
           })
           .catch(err => console.log(err.message))
 
-    },
+  },
+
 
     submitTotalValue() {
 
-      let keyMetricAsset = {totalValue: this.totalValuePortfolio, totalGain: this.totalGainInPortfolio}
+      let keyMetricAsset = {totalValue: this.totalValuePortfolio, totalGain: this.totalGainInPortfolio};
+
+      this.totalValues.push(this.totalValue);
 
       axios.post('http://localhost:5050/metrics', keyMetricAsset)
           .then(() => this.fetchMetrics())
@@ -212,10 +238,19 @@ export default {
       togglePortfolio() {
         this.showDetails = !this.showDetails;
       },
+    toArrayGains(){
+      for(let i = 0; i < this.metrics.length; i++){
+        this.totalValues.push(this.metrics[i].totalValue)
+      }
+
+    }
+
     },
     mounted() {
       this.fetchData();
       this.fetchMetrics();
+
+
     },
 
     computed: {
@@ -296,28 +331,71 @@ export default {
         }
         return gain.toFixed(1);
 
+      },
+      totalGainOfStocksInPortfolio() {
+        let gainStocks = 0;
+
+
+        return gainStocks.toFixed(1);
+
       }
-    }
+
+
+
+
+
+      }
+
+
+
 
 }
 </script>
 
 <style scoped>
 
+th, td {
+  padding: 10px;
+  text-align: left;
+}
+tr:hover {background-color: #f5f5f5;}
 
+.column-header{
+  font-weight: bold ;
+}
+.row-header{
+  font-weight: bold
+}
 
-#overview{
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+div.overview {
+  max-width: 500px;
+  float: left;
+  margin: auto;
+  padding-left: 45px;
+}
+
+.bottom-line{
+  border-top: 2px solid black;
+  font-weight: bold ;
 }
 
 .chart-container{
   max-width: 500px;
+  float: right;
+  padding-right: 45px;
 }
+.orange{
+  color: orange;
+}
+.green{
+  color: green;
+}
+
+.red{
+  color: red;
+
+}
+
 
 
 </style>
